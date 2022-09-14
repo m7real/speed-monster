40598f47646dcc5df4814d1c38fc357e298f1aa4
m7real <masayeedreal@gmail.com>
2022-09-14 21:55:47 +0600

    show second fixed

--- script.js
+++ script.js
@@ -133,5 +133,5 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? Math.ceil(timeSpent) : 0} seconds`;
 }, 1000);
